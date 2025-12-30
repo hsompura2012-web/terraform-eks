b2d8de9647dd631a96c0a30eae6a8306e05aadaa
Harshit <hsompura2012@gmail.com>
2025-12-30 13:40:53 +0530

    Update

--- main.tf
+++ main.tf
@@ -42,3 +42,19 @@ resource "aws_eks_node_group" "main" {
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
 
 }
+
+resource "aws_eks_access_entry" "main" {
+  cluster_name      = aws_eks_cluster.main.name
+  principal_arn     = "arn:aws:iam::501375286327:role/workstation-role"
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "main" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::501375286327:role/workstation-role"
+
+  access_scope {
+    type       = "cluster"
+     }
+}
