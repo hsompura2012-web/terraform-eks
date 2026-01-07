a591319ffd1d31671ece6968ffe95dbe2ec31163
Harshit <hsompura2012@gmail.com>
2026-01-07 17:43:10 +0530

    Update

--- main.tf
+++ main.tf
@@ -69,3 +69,48 @@ resource "null_resource" "kubeconfig" {
       }
 
 }
+
+resource "aws_instance" "instance_launch" {
+  for_each = var.Instance_Det
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = each.key
+  }
+
+}
+
+resource "aws_route53_record" "Record_Launch" {
+  for_each =  var.Instance_Det
+  zone_id = var.zone_id
+  name    = "${each.key}-dev"
+  type    = "A"
+  ttl     = 50
+  records = [aws_instance.instance_launch[each.key].private_ip]
+}
+
+resource "null_resource" "Instance_null" {
+  depends_on = [
+
+    aws_instance.instance_launch,
+    aws_route53_record.Record_Launch
+  ]
+
+  for_each = var.Instance_Det
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance_launch[each.key].private_ip
+    }
+    inline =  [
+      "sudo dnf install python3.13-pip -y",
+      "sudo pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/hsompura2012-web/Practice-Ansible-template-V1.git main.yaml -e component=${each.key} -e env=dev"
+
+    ]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,30 @@ variable "env" {
 }
 
 
+
+variable "ami" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids"{
+  default = [ "sg-07306e019683163e7" ]
+
+}
+
+variable "zone_id" {
+  default =  "Z055196614WSVQYU4VYMA"
+
+}
+
+variable "Instance_Det" {
+  default = {
+    mongodb = ""
+    redis = ""
+    mysql = ""
+    rabbitmq = ""
+      }
+}
